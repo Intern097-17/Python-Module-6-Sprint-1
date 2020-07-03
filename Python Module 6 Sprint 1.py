2cab8d483940a5dca8c918cd427720e6987e7fa4
Intern097-17 <62545643+Intern097-17@users.noreply.github.com>
2020-07-03 13:00:09 +0200

    Add files via upload

---
+++ Python Module 6 Sprint 1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pandas as pd 
+  
+
+df = pd.DataFrame({'Chips':['Simba', 'Lays'], 
+                    'Cooldrinks':['Coke', 'Fanta'], 
+                    'Chocolates':['Cadbury', 'Tex'],
+                    'Pies':['Pepper Steak', 'Chicken'],
+                   'Fruit':['Pear', 'Apple'],
+                   'Cupcakes':['Vanilla', 'Chocolate'],
+                   'Veggies':['Spinach', 'Cabbage']
+                  }) 
+  
+
+print(df) 
+
+
+# In[ ]:
+
+
+
+
